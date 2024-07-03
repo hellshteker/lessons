9f598b3f6fbcca4e659100363b76002f4202fc9c
hellshteker <153329333+hellshteker@users.noreply.github.com>
2024-07-03 10:55:45 +0300

    Add files via upload

---
+++ lesson_20/js/script.js
@@ -0,0 +1,31 @@
+"use strict"
+
+
+document.addEventListener("click", documentActions);
+
+function documentActions(e) {
+	const targetElement = e.target;
+
+	if (targetElement.closest('.menu__burger')) {
+		document.documentElement.classList.toggle('menu-open');
+	}
+
+	if (targetElement.closest('[data-goto]')) {
+		document.documentElement.classList.contains('menu-open') ?
+		document.documentElement.classList.remove('menu-open') : null;
+	}
+
+	if (targetElement.closest('[data-goto]')) {
+		const goTo = targetElement.closest('[data-goto]').dataset.goto;
+		const goToElement = document.querySelector(goTo);
+		const headerHeight = document.querySelector('.header').offsetHeight;
+
+		if (goToElement) {
+			window.scrollTo({
+				top: goToElement.offsetTop - (headerHeight + 15),
+				behavior: "smooth"
+			});
+		}
+		e.preventDefault();
+	}
+}
