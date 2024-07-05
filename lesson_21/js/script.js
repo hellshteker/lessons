6c9e2fc5c92b5aa2d9d028f1642cae4777d3ee08
hellshteker <153329333+hellshteker@users.noreply.github.com>
2024-07-05 10:59:07 +0300

    Add files via upload

---
+++ lesson_21/js/script.js
@@ -0,0 +1,11 @@
+"use strict"
+
+/* const userName = 20;
+userName = "Іштван";
+console.log(userName); */
+
+let numOne = 20;
+let numTwo = "20";
+
+let numSumm = numOne + numTwo;
+console.log(numSumm);
