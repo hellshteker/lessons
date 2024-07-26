93c95f901a8c0c87a518aedf3a8b764bf7ce9318
hellshteker <153329333+hellshteker@users.noreply.github.com>
2024-07-26 16:27:42 +0300

    Add files via upload

---
+++ lesson_22/js/script.js
@@ -0,0 +1,49 @@
+"use strict"
+
+/* const userName = 20;
+userName = "Іштван";
+console.log(userName); */
+
+
+
+
+/* for (let i = 1; i <= 10; i++) {
+    console.log(`Пункт №${i}`)
+} */
+
+
+/* if (2 * 20 <= 10 || 30 / 2 < 5 && 10 <= "10" || 20 === "20") {
+	console.log('000');
+}
+if (40 <= 10 || 15 < 5 && 10 <= "10" || 20 === "20") {
+	console.log('000');
+}
+if (false || false && true || false) {
+	console.log('000');
+}
+if (false || false || false) {
+	console.log('000');
+}
+if (false) {
+	console.log('000');
+} */
+
+
+/* function divide(a, b) {
+    if (a && b) {
+        return console.log(`Результат ділення: ${a / b}`)
+    }
+}
+
+divide(1, 2) */
+
+
+/* let array = ["Черешня", "10", "Шмаркач", 10, "Ще побігає"]
+
+array.forEach(function(item) {
+    if (item === 10) {
+        console.log (item)
+    }
+}) */
+
+
